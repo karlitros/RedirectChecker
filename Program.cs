bedc520c34760f767758a229d92fdd5208f11bcd
Karl <karlitros@gmail.com>
2023-08-02 15:40:00 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    /// <summary>
+    /// Fires requests to the 301 original urls, sees if it returns a 404 or not. If a 404, print it out.
+    /// </summary>
+    /// <param name="args">args[0] is the rewritemaps.xml filename: eg C:\\Development\\website\\config\\IISUrlRewriteMaps.xml. args[1] is the root url: eg https://mywebsite.co.uk</param>
+    private static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Provide at least 2 args. Rewrite maps xml file, and root url to fire requests against");
+            return;
+        }
+
+        var checker = new RedirectChecker.RedirectChecker(args[0], args[1]);
+
+        checker.ProcessRepositories();
+
+        Console.ReadLine();
+    }
+}
---
+++ RedirectChecker.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Http.Headers;
+using System.Xml;
+
+namespace RedirectChecker
+{
+    public class RedirectChecker
+    {
+        readonly string? _redirectFilePath;
+        readonly string? _rootUrl;
+
+        public RedirectChecker(string redirectFilePath, string rootUrl)
+        {
+            _redirectFilePath = redirectFilePath;
+            _rootUrl = rootUrl;
+        }        
+
+        public void ProcessRepositories()
+        {
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var file = File.Open(_redirectFilePath, FileMode.Open);
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            Console.WriteLine($"Loading your redirect file {_redirectFilePath}\r\n\r\n");
+            xmlDoc.Load(file);
+
+            var list = xmlDoc.GetElementsByTagName("add");
+
+            Console.WriteLine($"Checking your redirects on {_rootUrl}\r\n\r\n");
+
+            for (int x = 0; x < list.Count; x++)
+            {
+                var item = list[x];
+
+                if(item == null)
+                {
+                    continue;
+                }
+
+                var originalUrl = item.Attributes["key"];
+                var destinationurl = item.Attributes["value"];
+
+                if(originalUrl == null || destinationurl == null)
+                {
+                    continue;
+                }
+
+                var statusCode = client.GetAsync(_rootUrl + originalUrl.Value).Result.StatusCode;
+
+                if(statusCode.ToString().ToLower().Equals("notfound"))
+                {
+                    Console.Write($"Couldn't redirect {originalUrl.Value} to {destinationurl.Value}\r\n");
+                }
+            }
+        }
+    }
+}
